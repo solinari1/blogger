dd82b4089bf93de48344211966beb26115139a1b
Marc-Olivier Doyon <marcohd@gmail.com>
2017-03-07 00:57:11 -0500

    Added tagging feature

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,13 @@
 module ArticlesHelper
 	
 	def article_params
-  		params.require(:article).permit(:title, :body)
+  		params.require(:article).permit(:title, :body, :tag_list)
+	end
+
+	def tag_list=(tags_string)
+		tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+		new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+  		self.tags = new_or_found_tags
 	end
 
 end
